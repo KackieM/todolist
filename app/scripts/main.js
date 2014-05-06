20e7f0a636379d4288de962b9ac38c243e2a997c
Katherine Mattison <kbmedisto@gmail.com>
2014-05-06 17:09:14 -0400

    working on todo list

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("form").on("submit", function(event){
 		event.preventDefault();
 		var myNewTask = $(".listInput").val();
-		$(".taskList").prepend("<li>" + myNewTask + "</li>");
+		$(".taskList").append("<li>" + myNewTask + "</li>");
 	
 	});
 	
