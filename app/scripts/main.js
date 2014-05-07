37a54a96cb80e88e2950b7b4f7b7013e8d5b32fa
Katherine Mattison <kbmedisto@gmail.com>
2014-05-07 19:07:57 -0400

    changing to women who code and updating images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,22 @@
+// $(document).ready(function() {
+// 	// $("http://tiy-fee-rest.herokuapp.com/collections/kat")
+
+// 	$("form").on("submit", function(event){
+// 		event.preventDefault();
+// 		var myNewTask = $(".listInput").val();
+// 		var taskArrayObj = {
+// 			todo: myNewTask
+// 		};
+		
+// 		taskArray.push(taskArrayObj);
+
+// 		$(".taskList").append("<li>" + myNewTask + "</li>");
+	
+// 	});
+	
+// });
+
+
 $(document).ready(function() {
 	$("form").on("submit", function(event){
 		event.preventDefault();
